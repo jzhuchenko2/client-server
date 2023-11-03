2f6e87aa3a2f13f2b772efaaec4b19df6625b69c
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-11-03 01:07:23 -0500

    Update and rename Server to Server.py

--- Server.py
+++ Server.py
@@ -4,3 +4,8 @@ import threading
 def handle_client(client_socket):
 
 #function reads data from the client, checks if its "Hello"
+  request = client_socket.recv(1024)
+  if request.decode() == "Hello";
+    response = "Hi"
+else: 
+  response = "Goodbye"
