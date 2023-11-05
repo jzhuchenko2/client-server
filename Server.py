9de4b939719a476f50ce348ad3ef5234100bb91e
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-11-04 22:33:44 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -9,3 +9,16 @@ def handle_client(client_socket):
     response = "Hi"
 else: 
   response = "Goodbye"
+client_socket.send(response.encode())
+
+
+client_socket.close()
+
+def main():
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #the server listens for incoming connections on port 9500.
+
+    server.bind(("0.0.0.0", 9500))
+
+if __name__ == "__main__":
+  main()
